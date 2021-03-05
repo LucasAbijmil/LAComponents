c40a286c90c8a46db1937ac1dfb0fa4c5b19ffe0
Lucas Abijmil <lucas.abijmil@gmail.com>
2021-03-05 21:39:18 +0100

    Add onDismiss for hud modifier

--- Sources/LAComponents/SwiftUI ViewModifiers/Hud.swift
+++ Sources/LAComponents/SwiftUI ViewModifiers/Hud.swift
@@ -14,10 +14,12 @@ fileprivate struct HUD<Content: View>: View {
 
   @Environment(\.colorScheme) var colorScheme
   let content: Content
+  let onDismiss: (() -> Void)?
 
   // TO DO : Remove me in 5.4 and add @ViewBuilder before let content: Content
-  init(@ViewBuilder content: () -> Content) {
+  init(@ViewBuilder content: () -> Content, onDismiss: (() -> Void)?) {
     self.content = content()
+    self.onDismiss = onDismiss
   }
 
   var body: some View {
@@ -28,6 +30,7 @@ fileprivate struct HUD<Content: View>: View {
       .clipShape(Capsule())
       .shadow(color: Color.black.opacity(0.15), radius: 15, x: 0, y: 5) // TO DO : rework me
       .animation(.default)
+      .onDisappear(perform: onDismiss)
   }
 }
 
@@ -42,12 +45,12 @@ public extension View {
   /// - Parameters:
   ///   - isPresented: A binding to whether the heads-up display is presented.
   ///   - content: A closure returning the content of the heads-up display.
-  func hud<Content: View>(isPresented: Binding<Bool>, @ViewBuilder content: () -> Content) -> some View {
+  func hud<Content: View>(isPresented: Binding<Bool>, onDismiss: (() -> Void)? = nil, @ViewBuilder content: () -> Content) -> some View {
     ZStack(alignment: .top) {
       self
 
       if isPresented.wrappedValue {
-        HUD(content: content)
+        HUD(content: content, onDismiss: onDismiss)
           .transition(AnyTransition.move(edge: .top).combined(with: .opacity))
           .onAppear {
             DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
@@ -65,12 +68,12 @@ public extension View {
   ///   - item: A binding to an optional source of truth for the heads-up display. When representing a non-`nil` item, the system uses `content` to create a heads-up display representation of the item.
   ///     If the identity changes, the system dismisses a currently-presented heads-up display and replaces it with a new heads-up display.
   ///   - content: A closure returning the content of the heads-up display.
-  func hud<Item: Identifiable, Content: View>(item: Binding<Item?>, @ViewBuilder content: () -> Content) -> some View {
+  func hud<Item: Identifiable, Content: View>(item: Binding<Item?>, onDismiss: (() -> Void)? = nil, @ViewBuilder content: () -> Content) -> some View {
     ZStack(alignment: .top) {
       self
 
       if let _ = item.wrappedValue {
-        HUD(content: content)
+        HUD(content: content, onDismiss: onDismiss)
           .transition(AnyTransition.move(edge: .top).combined(with: .opacity))
           .onAppear {
             DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
