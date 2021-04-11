6c8b3f0c9fb2ef5a74b40f29e9348633e5a30631
Lucas Abijmil <lucas.abijmil@gmail.com>
2021-04-11 15:06:56 +0200

    Rework documentation for OS Modifiers

--- Sources/LAComponents/SwiftUI ViewModifiers/OS Modifiers/iOS.swift
+++ Sources/LAComponents/SwiftUI ViewModifiers/OS Modifiers/iOS.swift
@@ -7,7 +7,10 @@ import SwiftUI
 @available(iOS 13.0, macOS 11.0, tvOS 13.0, watchOS 6.0, *)
 public extension View {
 
-  /// A method that allows to apply modifiers only for iOS.
+  /// Executes a closure that allows to apply a `ViewModifier` on a `View` only for iOS.
+  ///
+  /// - Parameters:
+  ///   - modifier: The transformation to be applied to the `View` passed as a parameter to the closure if the os is iOS.
   func iOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
     #if os(iOS)
     return modifier(self)
--- Sources/LAComponents/SwiftUI ViewModifiers/OS Modifiers/macOS.swift
+++ Sources/LAComponents/SwiftUI ViewModifiers/OS Modifiers/macOS.swift
@@ -7,7 +7,10 @@ import SwiftUI
 @available(iOS 13.0, macOS 11.0, tvOS 13.0, watchOS 6.0, *)
 public extension View {
 
-  /// A method that allows to apply modifiers only for macOS.
+  /// Executes a closure that allows to apply a `ViewModifier` on a `View` only for macOS.
+  ///
+  /// - Parameters:
+  ///   - modifier: The transformation to be applied to the `View` passed as a parameter to the closure if the os is macOS.
   func macOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
     #if os(macOS)
     return modifier(self)
